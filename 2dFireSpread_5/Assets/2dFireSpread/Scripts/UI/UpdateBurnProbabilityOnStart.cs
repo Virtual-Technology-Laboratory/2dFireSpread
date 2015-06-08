78aaed6600572e43b5e16024eeba9366f3cfd91f
Roger Lew <rogerlew@gmail.com>
2015-06-07 19:57:56 -0700

    working again
    Library was corrupted.

---
+++ 2dFireSpread_5/Assets/2dFireSpread/Scripts/UI/UpdateBurnProbabilityOnStart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpdateBurnProbabilityOnStart : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var value = GetComponent<Slider>().value;
+        Transform.FindObjectOfType<FireSpreadManager>().SetBurnProbability(value);
+    }
+}
---
+++ 2dFireSpread_5/Assets/VTL/UI/SliderValueTextUpdate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderValueTextUpdate : MonoBehaviour
+{
+
+    Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = transform.FindChild("Handle Slide Area")
+                        .FindChild("Handle")
+                        .FindChild("Text")
+                        .GetComponent<Text>();
+
+        GetComponent<Slider>().onValueChanged.AddListener((value) => OnValueChange(value));
+
+        text.text = GetComponent<Slider>().value.ToString("P1");
+    }
+
+    // Update is called once per frame
+    public void OnValueChange(float value)
+    {
+        text.text = value.ToString("P1");
+    }
+}
